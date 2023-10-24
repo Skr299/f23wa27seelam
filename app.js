136819244a44b17612307c92d28037189bc0be33
Skr299 <S567427@nwmissouri.edu>
2023-10-23 23:41:55 -0500

    Made Changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
-var mycomputationRouter = require('./routes/Computation');
+var mycomputationRouter = require('./routes/computation');
 
 var app = express();
 
