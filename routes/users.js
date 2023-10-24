cb7cae89b11aec2c2e52b2b1ae866027f4f9a361
Skr299 <S567427@nwmissouri.edu>
2023-10-23 19:47:26 -0500

    users count

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var time = 0;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  time += 1;
+
+  res.send(`Sum is: ${time}`);
 });
 
 module.exports = router;
